c4b3a0ff17113f40f3984ee2151ae748324f1441
Alexander derck Horlings <horlingsalexander@gmail.com>
2022-10-12 21:28:08 +0200

    changing colour values

--- sources/colours.c
+++ sources/colours.c
@@ -6,7 +6,7 @@
 /*   By: ahorling <ahorling@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/10 15:19:21 by ahorling      #+#    #+#                 */
-/*   Updated: 2022/10/12 21:23:48 by ahorling      ########   odam.nl         */
+/*   Updated: 2022/10/12 21:27:06 by ahorling      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ int encode_rgba_julia(t_info info, t_comp pixel)
 	t_rgb	rgb;
 
 	rgb.a = 255;
-	rgb.r = info.iteration * 1;
+	rgb.r = info.iteration * 3;
 	rgb.g = info.iteration * 2;
-	rgb.b = info.iteration * 3;
+	rgb.b = info.iteration * 0.5;
 	return (rgb.r << 24 | rgb.g << 16 | rgb.b << 8 | rgb.a);
 }
 
